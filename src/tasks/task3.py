f8e4bdfd37f2863c00f14928b8bbb820888fcd3b
HectorVaq <82213319+HectorVaq@users.noreply.github.com>
2021-04-16 14:22:05 +0200

    Update task3.py

--- src/tasks/task3.py
+++ src/tasks/task3.py
@@ -1,3 +1,4 @@
+import numpy as np
 import time
 import easygopigo3 as easy
 
@@ -18,11 +19,16 @@ def safety_distance():
 def obstacle():
     return (my_distance_sensor.read_mm() <= dist*10)
 
+def wall():
+    return (my_distance_sensor.read_mm() <= l*10)
+
 
 
 veryfying = False
 detected = False
 dgr=360//n
+alpha=(180-90-dgr/2)
+l=2*(dist+5)*np.cos(alpha)+12
 
 while not detected:
     while safety_distance() and not veryfying:
@@ -32,16 +38,21 @@ while not detected:
             veryfying = True
             scans=1
     while safety_distance() and veryfying:
-        gpg.turn_degrees(90) #right: to put gpg on circular path
-        gpg.orbit(-dgr, dist+5)
-        gpg.turn_degrees(-90) #left: to focus on obstacle direction
-        if obstacle() and scans < n:
-            scans+=1
-            veryfying = True
-        else:
+        gpg.turn_degrees(alpha)
+        if wall():
+            gpg.turn_degrees(90)
             veryfying = False
+        else:
+            gpg.turn_degrees(90-alpha) #right: to put gpg on circular path
+            gpg.orbit(-dgr, dist+5)
+            gpg.turn_degrees(-90) #left: to focus on obstacle direction
+            if obstacle() and scans < n:
+                scans+=1
+                veryfying = True
+            else:
+                veryfying = False
     while not safety_distance():
-        gpg.turn_degrees(30)
+        gpg.turn_degrees(90)
     if scans==n:
         detected = True
 if detected:
