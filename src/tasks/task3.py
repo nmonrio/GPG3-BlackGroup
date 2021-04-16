a6c9877631869f2a67f8e51c184d87992d014bbe
HectorVaq <82213319+HectorVaq@users.noreply.github.com>
2021-04-16 19:46:30 +0200

    Scans protection

--- src/tasks/task3.py
+++ src/tasks/task3.py
@@ -8,6 +8,9 @@ my_distance_sensor = gpg.init_distance_sensor('AD2')
 
 #Sentinel
 n=int(input("Enter the number of scans required--->"))
+while n<4:
+    print("Minimum value admitted is 4")
+    n=int(input("Enter the number of scans required--->"))
 dist=int(input("Enter the minimum distance at which the obstacle is detected (between 10/20 cm)--->"))
 while dist<10:
     print("Not valid entry")
@@ -26,7 +29,7 @@ def wall():
 
 veryfying = False
 detected = False
-dgr=360//n
+dgr=360/n
 alpha=(180-90-dgr/2)
 l=2*(dist+5)*np.cos(alpha)+12
 
@@ -42,11 +45,11 @@ while not detected:
         if wall():
             gpg.turn_degrees(90)
             veryfying = False
-        else:
+        if not wall():
             gpg.turn_degrees(90-alpha) #right: to put gpg on circular path
             gpg.orbit(-dgr, dist+5)
             gpg.turn_degrees(-90) #left: to focus on obstacle direction
-            if obstacle() and scans < n:
+            if obstacle() and scans<n:
                 scans+=1
                 veryfying = True
             else:
