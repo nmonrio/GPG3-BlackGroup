91a7faa380339e40af5fe40198e57b493976f637
Eduardo Castro <100452301@alumnos.uc3m.es>
2021-04-22 11:26:07 +0200

    Update task1.py

--- src/tasks/task1.py
+++ src/tasks/task1.py
@@ -3,8 +3,13 @@ import math
 from easygopigo3 import EasyGoPiGo3
 import easygopigo3 as easy
 gpg = easy.EasyGoPiGo3()
+my_distance_sensor = gpg.init_distance_sensor('AD2')
 
 gpg = EasyGoPiGo3()  # Create object instance of the robot
+
+def destination_free():
+    condition = (my_distance_sensor.read_mm() >= (length+12)*10)
+    return condition
 
 # Drawing a square:
 length = 30  # Side length
@@ -53,12 +58,19 @@ legnth_hypotenuse = math.sqrt((length_one_side**2)+(length_other_side**2)))
 time.sleep(5)
                      
 # Drawing a n-sided polygon given the number of sides:
+                     
+                    
 n = int(input("Introduce the number of sides of the polygon"))
 length = int(input("Introduce the length of the sides in cm"))
-  
+alpha = 360/n
+                     
 for v in range(n):
-    gpg.drive_cm(length)
-    gpg.turn_degrees(360/n) 
+    if destination_free():
+                 gpg.drive_cm(length)
+                 gpg.turn_degrees(alpha)
+    else:
+                 gpg.turn_degrees(90)
+    
     
 
         
