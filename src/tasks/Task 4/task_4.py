2eca976bc3e01123edb94969ed6a93196b04f3a5
maxaurio <82219084+maxaurio@users.noreply.github.com>
2021-04-23 19:49:40 +0200

    hola

--- src/tasks/Task 4/task_4.py
+++ src/tasks/Task 4/task_4.py
@@ -7,7 +7,7 @@ actions = []
 def parse_command(command):
     tokens = command.split()
     structured_command = {}
-    name = tokens[0]
+    name = tokens[0].upper()
     parameters = tokens[1::]
     structured_command["NAME"] = name
     structured_command["PARAMETERS"] = parameters
@@ -17,13 +17,13 @@ def is_valid_command(command):
     is_valid = False
     if structured_command["NAME"] == "SET":
         is_valid = len(structured_command["PARAMETERS"]) == 2
-        is_valid = is_valid and structured_command["PARAMETERS"][0] == "SPEED"
+        is_valid = is_valid and structured_command["PARAMETERS"][0].upper() == "SPEED"
         is_valid = is_valid and structured_command["PARAMETERS"][1].isnumeric()
-        is_valid = is_valid and structured_command["PARAMETERS"][1] < 100
-        is_valid = is_valid and structured_command["PARAMETERS"][1] > 0
+        is_valid = is_valid and int(structured_command["PARAMETERS"][1]) < 100
+        is_valid = is_valid and int(structured_command["PARAMETERS"][1]) > 0
     
     if structured_command["NAME"] == "MV":
-        is_valid = structured_command["PARAMETERS"][0] == "R" or structured_command["PARAMETERS"][0] == "L" or structured_command["PARAMETERS"][0] == "B" or structured_command["PARAMETERS"][0] == "F"
+        is_valid = structured_command["PARAMETERS"][0].upper() == "R" or structured_command["PARAMETERS"][0].upper() == "L" or structured_command["PARAMETERS"][0].upper() == "B" or structured_command["PARAMETERS"][0].upper() == "F"
     if structured_command["NAME"] == "STOP":
         is_valid = True
     return is_valid
@@ -36,16 +36,16 @@ def execute_command(command):
     if structured_command["NAME"] == "STOP":
         gpg.stop()
     if structured_command["NAME"] == "MV":
-        actions.append("MV"+str(structured_command["PARAMETERS"]))
-        if structured_command["PARAMETERS"][0] == "R":
+        #actions.append("MV"+str(structured_command["PARAMETERS"]))
+        if structured_command["PARAMETERS"][0].upper() == "R":
             gpg.turn_degrees(90)
             gpg.forward()
-        if structured_command["PARAMETERS"][0] == "L":
+        if structured_command["PARAMETERS"][0].upper() == "L":
             gpg.turn_degrees(-90)
             gpg.forward()
-        if structured_command["PARAMETERS"][0] == "F":
+        if structured_command["PARAMETERS"][0].upper() == "F":
             gpg.forward()
-        if structured_command["PARAMETERS"][0] == "B":
+        if structured_command["PARAMETERS"][0].upper() == "B":
             gpg.turn_degrees(180)
             gpg.forward()
 
@@ -74,15 +74,14 @@ if __name__=="__main__":
         is_valid = is_valid_command(structured_command)
         if is_valid == True:
             execute_command(command)
-            gpg.sleep_ms(time)
+            
     if structured_command["NAME"] == "STOP":
         print("The actions you have done are: ")
-        for i in range(len(actions)):
-            if actions[i][0] == "MV":
-                print("The robot has moved in direction: "+str(structured_command["PARAMETERS"][0]))
-            if actions[i][0] == "SET":
-                print("The velocity was set to: "+str(structured_command["PARAMETERS"][1]))
-            if actions[i][0] == "STOP":
+        #print(actions)
+        for i in actions:
+            if i['NAME'] == "MV":
+                print("The robot has moved in direction: "+str(i["PARAMETERS"][0]))
+            if i['NAME'] == "SET":
+                print("The velocity was set to: "+str(i["PARAMETERS"][1]))
+            if i['NAME'] == "STOP":
                 print("The robot was stopped.")
-    
-    
