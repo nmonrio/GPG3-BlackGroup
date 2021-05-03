28c3cbd117f1f51129d72f56e9040ad128cac062
Eduardo Castro <100452301@alumnos.uc3m.es>
2021-05-03 13:09:47 +0200

    Update Task 1.py

--- src/Task 1/Task 1.py
+++ src/Task 1/Task 1.py
@@ -1,8 +1,13 @@
 import time
 import math
 from easygopigo3 import EasyGoPiGo3
+import easygopigo3 as easy
+gpg = easy.EasyGoPiGo3()
+my_distance_sensor = gpg.init_distance_sensor('AD1')
 
-gpg = EasyGoPiGo3()  # Create object instance of the robot
+def destination_free(l_cm):
+    condition = (my_distance_sensor.read_mm() >= l_cm*10)
+    return condition
 
 # Drawing a square:
 length = 30  # Side length
@@ -39,21 +44,27 @@ for i in range(3):
 time.sleep(5)
 
 # 2. Rectangular triangle
-length_one_side = 60
+length_one_side = 40
 length_other_side = 30
 length_hypotenuse = math.sqrt((length_one_side**2)+(length_other_side**2))
 gpg.drive_cm(length_one_side)
 gpg.turn_degrees(90)
 gpg.drive_cm(length_other_side)
-gpg.turn_degrees(180-math.atan(length_one_side/length_other_side))
+gpg.turn_degrees(180-(math.atan(length_one_side/length_other_side))*180/np.pi)
 gpg.drive_cm(length_hypotenuse)
 
 time.sleep(5)
                      
 # Drawing a n-sided polygon given the number of sides:
-n = int(input("Introduce the number of sides of the polygon\n"))
-length = int(input("Introduce the length of the sides in cm\n"))
-  
+                     
+                    
+n = int(input("Introduce the number of sides of the polygon"))
+length = int(input("Introduce the length of the sides in cm"))
+alpha = 360/n
+                     
 for v in range(n):
-    gpg.drive_cm(length)
-    gpg.turn_degrees(360/n)  
+    if destination_free(length):
+        gpg.drive_cm(length)
+        gpg.turn_degrees(alpha)
+    else:
+        gpg.turn_degrees(90)
