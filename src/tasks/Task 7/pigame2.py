eefdb2ec4d7c381fb8920953ba6db33812112391
maxaurio <82219084+maxaurio@users.noreply.github.com>
2021-05-06 19:22:52 +0200

    h

--- src/tasks/Task 7/pigame2.py
+++ src/tasks/Task 7/pigame2.py
@@ -1,57 +1,25 @@
-import pygame
-import sys
-from pygame.locals import *
+import sys, pygame
+pygame.init()
 
-BLACK = (0, 0, 0)
-WHITE = (200, 200, 200)
-WINDOW_HEIGHT = 400
-WINDOW_WIDTH = 400
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
 
-rows = []
-grid = []
+screen = pygame.display.set_mode(size)
 
+ball = pygame.image.load("robot.png")
+ballrect = ball.get_rect()
 
-def main():
-    global SCREEN, CLOCK
-    pygame.init()
-    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    icon = pygame.image.load("robot.png")
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
 
-    CLOCK = pygame.time.Clock()
-    SCREEN.fill(BLACK)
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
 
-    while True:
-        drawGrid()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == MOUSEBUTTONUP or event.type == MOUSEBUTTONDOWN:
-            		print(pygame.mouse.get_pos())
-            		pygame.display.update()
-
-        pygame.display.update()
-
-
-def drawGrid():
-    blockSize = WINDOW_WIDTH//n #Set the size of the grid block
-    for x in range(0, WINDOW_WIDTH, blockSize):
-        for y in range(0, WINDOW_HEIGHT, blockSize):
-            rect = pygame.Rect(x, y, blockSize, blockSize)
-            pygame.draw.rect(SCREEN, WHITE, rect, 1)
-
-def paint():
-	x, y = pygame.mouse.get_pos()
-	print(x, y)
-	return x, y
-
-if __name__=="__main__":
-	n = int(input("Tell me precission (5-50): "))
-	while n > 50 or n < 5:
-		n = int(input("Tell me precission (5-50): "))
-	for i in range(n):
-		rows.append(0)
-	for i in range(n):
-		grid.append(rows)
-	print(grid)
-	main()
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
--- src/tasks/Task 7/task_7.py
+++ src/tasks/Task 7/task_7.py
@@ -1,47 +1,85 @@
-import pygame as pg
+import pygame
 import sys
 from pygame.locals import *
-import numpy as np
 
-height = 1000
-width = 600
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 400
 
-pg.init()
+rows = []
+
+grid = []
 
 
+def main():
+    global SCREEN, CLOCK
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    icon = pygame.image.load("robot.png")
+    pygame.display.set_icon(icon)
+
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(BLACK)
+
+    running = True
+    while running == True:
+        drawGrid()
+        for event in pygame.event.get():
+            print("g")
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN: #or event.type == MOUSEBUTTONDOWN:
+            	print(pygame.mouse.get_pos())
+            	pygame.display.update()
+                clicar()
+            	print(grid)
+            elif event.type == pygame.KEYDOWN:
+                print(event.key)
+                if event.key == pygame.K_a:
+                    print("h")
+                    submit()
+                    pygame.display.update()
+        pygame.display.update()
+
+
+def drawGrid():
+    blockSize = WINDOW_WIDTH//n #Set the size of the grid block
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
+def hola(x, y):
+	celda = 400//n
+	position_x = x//celda+1
+	i = 1
+	position_y = y//celda+1
+	print(position_x)
+	print(position_y)
+	grid[position_x-1][position_y-1] = i
+
+def clicar():
+   # while event.type != MOUSEBUTTONUP:
+    a = pygame.mouse.get_pos()[0]
+    b = pygame.mouse.get_pos()[1]
+    hola(a, b)
+
+def submit():
+    print(grid)
+    running = False
+    pygame.quit()
+    sys.quit()
+    
 
 if __name__=="__main__":
-	n = int(input("Tell  me precission (5-50): "))
-	while n < 5 or n > 50:
-		n = int(input("Tell  me precission (5-50): "))
-	if n > 5 and n < 50:
-		robot = pg.image.load("robot.png")
-
-
-
-		screen = pg.display.set_mode((height, width))
-		pg.display.set_caption("Movement")
-		pg.display.set_icon(robot)
-
-
-		screen.fill(pg.Color("black"))
-
-		pg.display.update()
-		clock = pg.time.Clock()
-
-		i = 0
-		running = True
-		while running == True:
-			clock.tick(60) 
-			for event in pg.event.get(): 
-				if event.type == pg.QUIT: sys.exit()
-				elif event.type == MOUSEBUTTONDOWN or event.type == MOUSEBUTTONUP:
-					#running = False
-
-					#x, y = 128, 128
-					robot = pg.transform.scale(robot, (128, 128))
-					screen.blit(robot, (64, 64))
-					pg.display.update()
-				pg.display.update()
-			pg.display.update()
-		pg.display.update()
+	n = int(input("Tell me precission (5-50): "))
+	while n > 50 or n < 5:
+		n = int(input("Tell me precission (5-50): "))
+	for i in range(n):
+		rows.append(0)
+	for i in range(n):
+		grid.append(rows.copy())
+	print(grid)
+	main()
