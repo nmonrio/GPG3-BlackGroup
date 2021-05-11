78afefeafbc5c777ae577db159244b02ff2cb1e1
Eduardo Castro <100452301@alumnos.uc3m.es>
2021-05-11 18:54:44 +0200

    Update Task 2 definitive.py

--- src/tasks/Task 2 definitive.py
+++ src/tasks/Task 2 definitive.py
@@ -1,1 +1,94 @@
+import time
+import math
+from easygopigo3 import EasyGoPiGo3
+import easygopigo3 as easy
+gpg = easy.EasyGoPiGo3()
+my_distance_sensor = gpg.init_distance_sensor('AD1')
 
+list = []
+
+def movement(i):
+    gpg.drive_cm(i)
+    actions.append("The robot has moved " + str(i) + "cm")
+
+def turn(i):
+    gpg.turn_degrees(i)
+    actions.append("The robot has turned " + str(i) + "degrees")
+   
+def orbita(i,j):
+    gpg.orbit(i,j)
+    actions.append("The robot has orbited " + str(i) + "degrees")
+  
+def print_actions():
+    print("Velocity has not changed")
+    for i in range(len(list)):
+        print("You have" + str(actions[i]))
+
+
+def destination_free(l_cm):
+    condition = (my_distance_sensor.read_mm() >= l_cm*10)
+    return condition
+
+command = int(input("Enter your command"))
+
+# Drawing a square:
+if command == 1:
+    length = int(input("Enter the length of the side"))  # Side length
+    for i in range(4):
+        movement(length)  # Drive forward for length cm
+        turn(90)  # Rotate 90 degrees to the right
+
+# Drawing a circle:
+# uses the .orbit(D, R) command where D is the degrees you'll be
+# rotating and R the radius [cm] of said circle.
+elif command == 2: #circle
+    radius = int(input("Enter the radius"))
+    orbita(360,40)
+
+#Rectangle
+elif command == 3:
+    short_side = int(input("Enter the  length of the short side"))
+    long_side = int(input("Enter the length of the long side"))
+    for i in range(2):
+        movement(short_side) 
+        turn(90)
+        movement(long_side) 
+        turn(90)
+
+
+# Drawing triangles:
+# 1. equilateral triangle of side length 'length'
+elif command == 4:
+    length = int(input("Enter the length of the side")
+    for i in range(3):
+    movement(length) 
+    turn(120)
+
+
+                
+# 2. Rectangular triangle
+elif command == 5:
+    length_one_side = int(input("Enter the length of one side"))
+    length_other_side = int(input("Enter length of one side"))
+    length_hypotenuse = math.sqrt((length_one_side**2)+(length_other_side**2))
+    movement(length_one_side)
+    turn(90)
+    movement(length_other_side)
+    turn(180-(math.atan(length_one_side/length_other_side))*180/np.pi)
+    movement(length_hypotenuse)
+
+                     
+# Drawing a n-sided polygon given the number of sides:
+                     
+elif command == 6:                  
+    n = int(input("Introduce the number of sides of the polygon"))
+    length = int(input("Introduce the length of the sides in cm"))
+    alpha = 360/n
+    for v in range(n):
+        if destination_free(length):
+            movement(length)
+            turn(alpha)
+        else:
+            turn(alpha)
+        
+print_actions()   
